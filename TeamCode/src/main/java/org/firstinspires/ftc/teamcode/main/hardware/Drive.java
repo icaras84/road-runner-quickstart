9461ac42733d48ce2323c92e8299beaf9dc5889e
icaras84 <icaras84@gmail.com>
2021-12-15 15:54:25 -0800

    Added Drive and Main OpMode

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/main/hardware/Drive.java
@@ -0,0 +1,27 @@
+package org.firstinspires.ftc.teamcode.main.hardware;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+public class Drive extends OpMode {
+
+
+    @Override
+    public void init() {
+
+    }
+
+    @Override
+    public void start(){
+
+    }
+
+    @Override
+    public void loop() {
+
+    }
+
+    @Override
+    public void stop(){
+
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/main/opmode/MainTestOp.java
@@ -0,0 +1,28 @@
+package org.firstinspires.ftc.teamcode.main.opmode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+public class MainTestOp extends OpMode {
+
+
+
+    @Override
+    public void init() {
+
+    }
+
+    @Override
+    public void start(){
+
+    }
+
+    @Override
+    public void loop() {
+
+    }
+
+    @Override
+    public void stop() {
+
+    }
+}
